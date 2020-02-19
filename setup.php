436db4f41d874b554cff5ee88a8ece8e4a8207dd
Richard Kwame Selase <qwameselase@gmail.com>
2020-02-19 12:29:48 +0000

    product lists update for db list setup

--- setup.php
+++ setup.php
@@ -15,11 +15,13 @@
 	$db->init();
 
 	// Add some teams
+	$db->add_brand('Club Beer');
+	$db->add_brand('Alomo Bitters');
 	$db->add_brand('Kpoo Keke');
-	$db->add_brand('Vitamilk');
-	$db->add_brand('Bel Aqua Mineral Water');
-    $db->add_brand('Guinness Foreign Extra Stout');
-    $db->add_brand('Odehye3 Beer');
+    $db->add_brand('Verna Water');
+    $db->add_brand('ABC Golden Lager Beer');
+    $db->add_brand('Bel Active');
+    $db->add_brand('Voltic');
     /*$db->add_brand('Origin Beer1');
     $db->add_brand('Club Beer1');
     $db->add_brand('Star Beer1');
