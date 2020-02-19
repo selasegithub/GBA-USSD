65e546d05d4f8948cbccb546593acb8e46a82370
Richard Kwame Selase <qwameselase@gmail.com>
2020-02-19 14:14:24 +0000

    FIX error on mobile phones

--- db.php
+++ db.php
@@ -60,7 +60,7 @@ $port = '5432';*/
                 $this->db = new PDO('pgsql:host=ec2-54-197-232-203.compute-1.amazonaws.com;dbname=dfr4hedmq20d1m;user=kdyztookkckndr;port=5432;password=29846ef8b03eecd8ac209177adab65a0c73f8b3c4b92de2276dcd010b4f69539');
                 // set the PDO error mode to exception
                 $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "Connected successfully";
+                //echo "Connected successfully";
 
 
             }
