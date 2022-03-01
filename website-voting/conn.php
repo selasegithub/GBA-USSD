a332bd1c7d5388ebbcb3d02c33fb6aa29c384758
Richard Kwame Selase <qwameselase@gmail.com>
2022-03-01 12:09:43 +0000

    Test DB Conn

--- website-voting/conn.php
+++ website-voting/conn.php
@@ -5,15 +5,15 @@
 
 $db_url = getenv("DATABASE_URL") ?: "postgres://kdyztookkckndr:29846ef8b03eecd8ac209177adab65a0c73f8b3c4b92de2276dcd010b4f69539@ec2-54-197-232-203.compute-1.amazonaws.com:5432/dfr4hedmq20d1m
 ";
-echo "$db_url\n";
+//echo "$db_url\n";
 
 $db_connection = pg_connect($db_url);
-/*if($db_connection) {echo "connected";} else {echo "not connected";}
+if($db_connection) {echo "connected";} else {echo "not connected";}
 
 $selectSql = "SELECT 1";
 $result =  pg_query($db, $selectSql);
 
 while ($row = pg_fetch_row($result)) {
     var_dump($row);
-}*/
+}
 ?>
